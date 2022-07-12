2006b374df9e951e1c70cf1f25aed69f0ce398cc
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2022-07-12 00:28:15 -0300

    att

--- javascript/index.js
+++ javascript/index.js
@@ -60,6 +60,9 @@ function constructorChore() {
                         }
                     }
                 }
+                if (ev.classList.contains('icon-help')) {
+                    window.alert('Lista de tarefas com progresso, para adicionar tarefas click em: <ADD>, para marcar como concluido use: <DONE>, para desfazer seleção use <↩>, para remover selecionados use: <DEL>, para selecioar tudo use: <ALL>')
+                }
             });
         },
 
